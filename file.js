20854a9eb8eb9102bcba079678fb6d3f5d08fcf8
kv158 <108169728+kv158@users.noreply.github.com>
2022-07-25 12:05:09 +0530

    Update file.js

--- file.js
+++ file.js
@@ -2,6 +2,14 @@
     "userId": 1,
     "id": 1,
     "title":"this is thought of the day",
-     "story":"this is a sample story",
+     "story":" date= 25-jul-22
+  Laziness won’t get you anywhere
+  “In ancient times, a king had his men place a boulder on a roadway. He then hid in the bushes, and watched to see if anyone would move the boulder out of the way. Some of the king’s wealthiest merchants and courtiers passed by and simply walked around it.
+
+Many people blamed the King for not keeping the roads clear, but none of them did anything about getting the stone removed.
+
+One day, a peasant came along carrying vegetables. Upon approaching the boulder, the peasant laid down his burden and tried to push the stone out of the way. After much pushing and straining, he finally managed.
+
+After the peasant went back to pick up his vegetables, he noticed a purse lying in the road where the boulder had been. The purse contained many gold coins and note from the King explain that the gold was for the person who removed the boulder from the road.”",
      "completed": false
   }
